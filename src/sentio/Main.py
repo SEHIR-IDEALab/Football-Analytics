8cdec94f203042b8fea70d3bd2c972ddb2f5a277
Emrullah Delibas <emrullah.delibas@egemsoft.net>
2015-10-06 18:24:23 +0300

    main added

--- src/sentio/Main.py
+++ src/sentio/Main.py
@@ -2,6 +2,7 @@
 import time as tm
 import os
 from src.sentio.Parameters import DATA_BASE_DIR
+from src.sentio.Time import Time
 from src.sentio.file_io.Parser import Parser
 from src.sentio.file_io.Writer import Writer
 from src.sentio.file_io.reader.JSONreader import JSONreader
@@ -18,49 +19,66 @@ def main():
     # event_data = os.path.join(DATA_BASE_DIR, 'input/GS_FB_Event.txt')
 
 
-    reader = JSONreader(os.path.join(DATA_BASE_DIR, 'output/sentio_data.json'))
+    reader = XMLreader(os.path.join(DATA_BASE_DIR, 'output/sentio_data_new.xml'))
     game_instances, slider_mapping = reader.parse()
+
+    # count = 0
+    # for game_instance in game_instances.getAllInstances():
+    #     if game_instance.event:
+    #         count += 1
+    #         print game_instance.event
+    # print count
 
     # writer = Writer(game_instances, slider_mapping)
     # writer.createFileAsXML()
     # writer.createFileAsJSON()
 
 
-    # for i in parser.game_instances:
-    #     print i, parser.game_instances[i]
 
-    # game_events = file_io.getGameEvents()
-    # for event_time in sorted(game_events.keys()):
-    #     temp_game_event = game_events.get(event_time)
-    #     print temp_game_event.isPassEvent()
+    # for game_instance in game_instances.getFirstHalfInstances():
+    #     if game_instance.event and game_instance.event.isPassEvent():
+    #         pass_event = game_instance.event.pass_event
+    #         pass_source = pass_event.pass_source
+    #         pass_target = pass_event.pass_target
+    #         print game_instance.time.half, pass_source.get_position(), pass_target.get_position(), pass_event.isSuccessful()
 
-
-    #q = file_io.getCoordinateData()
-    #file_io.getCombinedEventData()
-    # file_io.getCoordinateData()
-
-    #for definedPass in sentio.getEventData():
-    # #    print definedPass
-    #
 
 
 
     match = Match(reader)
+    #
+    # match.getGameEvents()
+    # print match.computeGameStopTimeIntervals()
+
+    # match.buildMatchObjects()
+    # match.computeEventStats()
+    #
+    # team_players = match.getHomeTeam().getTeamPlayers()
+    #
+    # # for half in home_goal_keeper.coord_info:
+    # #     for milliseconds in home_goal_keeper.coord_info[half]:
+    # #         print half, milliseconds, home_goal_keeper.coord_info[half][milliseconds]
+    #
+    # print home_goal_keeper.computeRunningDistance()
+
+    # for team_player in team_players:
+    #     print "direction: ", team_player.calculateDirectionAtAllPoints()
+        # print team_player.calculateSpeed(Time(1, 500))
+        # print team_player.printStats()
+        # print "without filter: ", team_player.computeRunningDistance()
+        # print "with game_stop filter: ", team_player.computeRunningDistanceWithGameStopFilter()
+        # print "with game_stop and speed filter: ", team_player.computeRunningDistanceWithGameStopAndSpeedFilter()
+        # print "---------------------"
+        # break
+
+
     match.visualizeMatch()
-    #
 
 
 
 
 
 
-
-    #print match.getMatchScore_forGivenTime(2, 75, 56, 0)
-
-#    getBallOwner_byTime(1,0,0,0)
-
-    #
-    #
     # match.identifyObjects()
     # #match.compute_someEvents()
     # q = match.getHomeTeam().getTeamPlayers()
